bbff3d999cdb6935142ecce00365e4ddf1730e4d
Yessin <114745001+yessir00N@users.noreply.github.com>
2026-03-03 13:16:09 +0100

    logo
    Ik heb de ma logo op de header toe gevoegd

--- resources/views/templates/navigation.blade.php
+++ resources/views/templates/navigation.blade.php
@@ -1,13 +1,9 @@
  <header class="header" role="banner">
     <div class="header-inner">
         <div class="logo">
-            <div class="logo-icon" aria-hidden="true">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="3"
-                    stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                </svg>
+            <div class="logo-icon">
+                 <a href="/index" class="nav-item"><img src="/images/logos/ma_logo.png" alt="Ma logo"></a>
             </div>
-            <span class="logo-text">Logo</span>
         </div>
 
         <nav class="main-nav" role="navigation" aria-label="Hoofdnavigatie">
