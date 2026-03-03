0bebfbd69dd604ec906788437f693c59cafdbb2a
RyanSchorel <ryanschorel@gmail.com>
2026-03-03 13:17:48 +0100

    footer toegevoegt
    heb loren ipsum geadd
    moet nog footer op bodem fixen

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,2 +1,45 @@
 </body>
+<footer>
+<div>
+<h2>Logaar</h2>
+<ul>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+
+</ul>
+</div>
+
+<div>
+<h2>Onze producten</h2>
+<ul>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+
+</ul>
+</div>
+
+<div>
+<h2>Onze socials</h2>
+<ul>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+    <li><a href="">Lorem ipsum dolor sit</a></li>
+</ul>
+</div>
+
+<div>
+<h2>Overige links</h2>
+<ul>
+    <a href=https://github.com/Jedemajed4617/proef-proeve-van-bekwaamheid>Github</a></li>
+    
+
+</ul>
+</div>
+<div>
+<h2>Over ons</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus.</p>
+</div>
+</footer>
+
 </html>
