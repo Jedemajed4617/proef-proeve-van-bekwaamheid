2d3c5aeecda2dbfd5417f604fc0a0830bc93edfe
RyanSchorel <ryanschorel@gmail.com>
2026-03-04 09:40:03 +0100

    naviagtion request
    URI

--- resources/views/templates/navigation.blade.php
+++ resources/views/templates/navigation.blade.php
@@ -1,15 +1,18 @@
  <header class="header" role="banner">
-    <div class="header-inner">
-        <div class="logo">
-            <div class="logo-icon">
+     <div class="header-inner">
+         <div class="logo">
+             <div class="logo-icon">
                  <a href="/index" class="nav-item"><img src="/images/logos/ma_logo.png" alt="Ma logo"></a>
-            </div>
-        </div>
-        <nav class="main-nav" role="navigation" aria-label="Hoofdnavigatie">
-            <a href="#" class="nav-item active" aria-current="page">Home</a>
-            <a href="#" class="nav-item">Curriculum</a>
-            <a href="#" class="nav-item">Nieuws</a>
-            <a href="#" class="nav-item">Contact</a>
-        </nav>
-    </div>
-</header>
+             </div>
+         </div>
+         <nav class="main-nav" role="navigation" aria-label="Hoofdnavigatie">
+             <a href="/"
+                 class="nav-item {{ request()->segment(1) === null ? 'active' : '' }}">
+                 Home
+             </a>
+             <a href="#" class="nav-item">Curriculum</a>
+             <a href="#" class="nav-item">Nieuws</a>
+             <a href="#" class="nav-item">Contact</a>
+         </nav>
+     </div>
+ </header>
