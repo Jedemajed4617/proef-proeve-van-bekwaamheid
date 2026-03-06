f09ec858b891c66e59d2c10cb8d9530a6e8ff9a8
Tygo Iedema <tygoiedema@gmail.com>
2026-03-06 12:27:27 +0100

    Login klaar

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,21 +6,29 @@
     <div class="login_card" aria-labelledby="periode-heading">
         <form method="POST" action="/login/auth" class="login_form">
             @csrf
-            <label class="input_container_login">
-                <p id="anim_text">E-mail</p>
-                <input class="login_input" id="input_anim" type="email" name="email" required>
-            </label>
-            <label class="input_container_login">
-                <p id="anim_text2">Wachtwoord</p>
-                <input class="login_input" id="input_anim2" type="password" name="password" required>
-            </label>
-            <a href="/login/password_reset" class="">
-                Wachtwoord vergeten?
-            </a>
-            <label class="remember_login">
-                <input type="checkbox" name="remember" class="rounded">
-                Onthoud mij
-            </label>
+            <div class="form_title">
+                <h1>Inloggen in beheer</h1>
+                <p>Welkom terug, vul uw gegevens in.</p>
+            </div>
+            <div class="inputs_section">
+                <label class="input_container_login">
+                    <p id="anim_text">E-mail</p>
+                    <input class="login_input" id="input_anim" type="email" name="email" required>
+                </label>
+                <label class="input_container_login">
+                    <p id="anim_text2">Wachtwoord</p>
+                    <input class="login_input" id="input_anim2" type="password" name="password" required>
+                </label>
+            </div>
+            <div class="login_actions">
+                <a href="/login/password_reset" class="psw_forget">
+                    Wachtwoord vergeten?
+                </a>
+                <label class="remember_login">
+                    <input type="checkbox" name="remember" class="rounded">
+                    Onthoud mij
+                </label>
+            </div>
             <label class="login_submit_container">
                 <button type="submit" class="login_submit">Inloggen</button>
             </label>
@@ -37,7 +45,7 @@
 
       input.addEventListener("focus", () => {
         label.style.transition = "0.2s all";
-        label.style.top = "-30px";
+        label.style.top = "-25px";
         label.style.left = "0";
       });
 
